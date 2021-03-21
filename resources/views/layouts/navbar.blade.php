5f8a2e5aa48f73484eeab24056047c34d76edba8
Armando Jovani MArtinez Ruiz <armando.martinez.1738@gmail.com>
2021-03-20 20:04:01 -0600

    deploy portafolio

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -10,7 +10,7 @@
     <div class="navbar-collapse collapse justify-content-end" id="navbarDefault">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link js-scroll active" href="index.html">INICIO</a>
+          <a class="nav-link js-scroll active" href="/">INICIO</a>
         </li>
         <li class="nav-item">
           <a class="nav-link js-scroll " href="#about">SOBRE MÍ</a>
